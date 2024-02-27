240171c6631c40af8704b6ec159fb8cf26dda6d7
lukasmiljic <smiljicluka13@gmail.com>
2024-02-27 12:46:42 +0100

    added rating field

--- scripts/channelData.js
+++ scripts/channelData.js
@@ -10,6 +10,7 @@ export const channels = [
           "(Hrvatska) Dokumentarno-povijesna emisija, emitira se svakodnevno od 1976. godine. Prikazuje važne događaje i obljetnice iz hrvatske i svjetske povijesti.",
         Category: ["Edukativno"],
         Rerun: false,
+        rating: 1,
         channelTitle: "HRT1",
       },
       {
@@ -20,6 +21,7 @@ export const channels = [
           "Glumci: Dan Haggerty, Denver Pyle, Don Shanks Redatelj: Jack Hively Priča: Ray Goldrup, Malvin Wald (SAD, 1977) Adams i Ludi Jack pružaju zaklon za dvoje djece siročadi, Michele i Eugenea. Djeca su isprva zahvalna, ali ubrzo im dosadi pridržavati se pravila. Potajno odlutaju kako bi našli neku zabavu, no to bi ih oboje moglo dovesti u opasnost.",
         Category: ["Avanturistički", "Obiteljski", "Western"],
         Rerun: true,
+        rating: 1,
         channelTitle: "HRT1",
       },
       {
@@ -30,6 +32,7 @@ export const channels = [
           "Glumci: Clint Eastwood, Robert Duvall, John Saxon, Don Stroud, Stella Garcia, James Wainwright, Paul Koslo, Gregory Walcott, Lynne Marta, Joaquín Martínez, Dick van Patten, John Carter, Pepe Hern, Ron Soble, Pepe Callahan, Clint Ritchie, Gil Barreto, Ed Deemer, Maria Val, Chuck Hayward, Michael R. Horst, Steve Eastin, Doug Greenough, Archie Harrison, Rick Kahana, Tony Kahana, Mark Klinkowsky, Ron Manning, Fred McDougall, Steve Miller Redatelj: John Sturges Priča: Elmore Leonard",
         Category: ["Western", "Kauboj"],
         Rerun: false,
+        rating: 1,
         channelTitle: "HRT1",
       },
       {
@@ -40,6 +43,7 @@ export const channels = [
           "(Hrvatska) U ovotjednoj emisiji govorimo o životu reformirane kalvinske crkvene općine Korođ pored Osijeka. Središte njihova vjerskog života reformirana je crkvena općina Korođ čija crkva ove godine bilježi 150. godišnjicu izgradnje. Evanđeoska pentekosna crkva u Poreču ove godine obilježava 20. obljetnicu osnutka. Skromna je to zajednica koja broji 30-ak članova. Razgovarali smo s pastorom te crkve Janom Čiefom. U religijskom pojmovniku predstavljamo liturgijska ruha koja obilježavaju razne protestantske tradicije. Novo svjetlo na promišljanje Kuranskih poruka daje knjiga glavnog imama Medžlisa Islamske zajednice u Osijeku Senada Heveševića Kako Kuran odgaja. Obzorja emocija. Predstavljamo i dva autorska albuma Vladimira Hoblaja.",
         Category: ["Vjerski"],
         Rerun: false,
+        rating: 1,
         channelTitle: "HRT1",
       },
       {
@@ -50,6 +54,7 @@ export const channels = [
           "(Hrvatska, 2024) Mozaična emisija koja od ponedjeljka do subote donosi aktualne informativne i zabavne sadržaje, s voditeljskim parom u studiju i gostima te uz reportaže i javljanja uživo iz cijele Hrvatske i redovite rubrike sa savjetima iz svih područja života.",
         Category: ["Informativo"],
         Rerun: false,
+        rating: 1,
         channelTitle: "HRT1",
       },
       {
@@ -60,6 +65,7 @@ export const channels = [
           "(Hrvatska) Multinacionalni magazin, donosi aktualnosti o manjinama i ljudskim pravima.",
         Category: ["Informativo"],
         Rerun: false,
+        rating: 1,
         channelTitle: "HRT1",
       },
       {
@@ -70,6 +76,7 @@ export const channels = [
           "(Hrvatska) Dokumentarno-povijesna emisija, emitira se svakodnevno od 1976. godine. Prikazuje važne događaje i obljetnice iz hrvatske i svjetske povijesti.",
         Category: ["Informativno"],
         Rerun: false,
+        rating: 1,
         channelTitle: "HRT1",
       },
       {
@@ -80,6 +87,7 @@ export const channels = [
           "(Hrvatska, 2024) Aktualne informacije i najnovije vijesti iz zemlje i svijeta u podnevnom Dnevniku.",
         Category: ["Informativno"],
         Rerun: false,
+        rating: 1,
         channelTitle: "HRT1",
       },
       {
@@ -89,6 +97,7 @@ export const channels = [
         description: "(Hrvatska, 2024) Dnevni sportski izvještaji i reportaže.",
         Category: ["Informativno"],
         Rerun: false,
+        rating: 1,
         channelTitle: "HRT1",
       },
       {
@@ -99,6 +108,7 @@ export const channels = [
           "(Hrvatska, 2024) Nakon Dnevnika 1 slijedi vremenska prognoza, uz meteozanimljivosti i upozorenja na opasne vremenske pojave, a uoči Dnevnika 2 sažeta tjedna prognoza.",
         Category: ["Informativno"],
         Rerun: false,
+        rating: 1,
         channelTitle: "HRT1",
       },
     ],
@@ -114,6 +124,7 @@ export const channels = [
           "(Hrvatska) Organizacija gradilišta, koordiniranje, stalna komunikacija s građevinskim radnicima, samo su neki od poslova građevinskog poslovođe. Upoznajte poslovođu Josipa. Gost rubrike Notes je Matej Mihić s Građevinskog fakulteta u Zagrebu, a ujedno i predsjednik Hrvatske udruge koordinatora zaštite na radu. U Miljokazu vas vodimo na otok koji ima tek stotinjak stalnih stanovnika. Idemo na Zlarin. U Tips & Tricksima otkrivamo najčešće izazove pri ishođenju građevinske dozvole. I ovotjednu Koordinaciju započinjemo s Antinom garažom. Ondje nam je Ante Cash pripremio koncentrat znanja o parketima",
         Category: ["Informativno", "Zabava"],
         Rerun: false,
+        rating: 1,
         channelTitle: "HRT2",
       },
       {
@@ -124,6 +135,7 @@ export const channels = [
           "Redatelj: Daisy Newton-Dunn Priča: Monty Don (Jug, Engleska, 2020) Monty posjećuje poznati vrt predsjednika Thomasa Jeffersona, jednog od osnivača Sjedinjenih Država, te uživa u potrazi za sjemenjem i sadnicama za svoj povrtnjak. Odande putuje na jug, do Charlestona koji se diči nekim od najljepših vrsta drveća na svijetu te se detaljnije upoznaje o održavanju tamošnjih plantaža. U Miamiju posjećuje jednu od najspektakularnijih renesansnih replika izgrađenih na močvarnome tlu, a potom se pridružuje lovcu na orhideje u potrazi za rijetkim vrstama koje samo on zna kako pronaći.",
         Category: ["Dokumentarano", "Zabava"],
         Rerun: false,
+        rating: 1,
         channelTitle: "HRT2",
       },
       {
@@ -134,6 +146,7 @@ export const channels = [
           "(Kanada, 2021) Čovjek koji je nastrijelio Tya vraća se u Hudson i Amy zajedno s obitelji ne zna kako da reagira na to. Jessica zamoli pomoć od Katie i Parker kako bi riješila problem zahtjevnoga gosta s ranča za turiste jer je Lou na putu.",
         Category: ["Drama", "Obiteljski"],
         Rerun: true,
+        rating: 1,
         channelTitle: "HRT2",
       },
       {
@@ -144,6 +157,7 @@ export const channels = [
           "(Francuska, Australija, 2019) U Južnoj Americi smo kako bismo upoznali guanaka, divljeg rođaka domaće ljame, unesenog u obred udvaranja. Približavamo se slonićima koji uče važne životne lekcije pod budnim okom svojeg brižnog krda. Pripremimo se na paranje ušiju kad se ogrličasti pingvini jave svojim poznatim bučnim glasanjem. Zatim ćemo se zaputiti u sušni jugozapad SAD-a kako bismo proveli trenutke s jednim od rijetkih otrovnih guštera: otrovnim bradavičarom.",
         Category: ["Dokumentarac", "Edukativno"],
         Rerun: true,
+        rating: 1,
         channelTitle: "HRT2",
       },
       {
@@ -154,6 +168,7 @@ export const channels = [
           "(Engleska, 2019) Geordiejeva zabrinutost zbog budućnosti dođe do izražaja nakon smrti profesora u najmodernijem računalnom laboratoriju pa zamoli Leonarda za pomoć oko slučaja.",
         Category: ["Drama"],
         Rerun: true,
+        rating: 1,
         channelTitle: "HRT2",
       },
       {
@@ -164,6 +179,7 @@ export const channels = [
           "(Engleska, 2012) Nakon što dadilju Jessicu Lake nađu mrtvu, Lewis i Hathaway odmah si počinju postavljati brojna pitanja. Je li se umorstvo dogodilo slučajno? Jesu li uopće htjeli ubiti Jess budući da su se Nick i Honey Addams trebali vratiti te večeri, a i dadilja koja im inače čuva djecu otkazala je u posljednji tren? ",
         Category: ["Drama"],
         Rerun: true,
+        rating: 1,
         channelTitle: "HRT2",
       },
       {
@@ -173,6 +189,7 @@ export const channels = [
         description: "(Događaj u jesen, SAD, Engleska, ",
         Category: ["Krimi", "Drama"],
         Rerun: false,
+        rating: 1,
         channelTitle: "HRT2",
       },
       {
@@ -183,6 +200,7 @@ export const channels = [
           "(Francuska, 2021) Dnevni i noćni leptiri. Ljupki i lijepi, lepršaju i lebde proljetnim i ljetnim nebom. Njihove nježne koreografije u blistavim bojama među najčudesnijima su u životinjskom carstvu. Ali ljepota nije sve što posjeduju. Lećama snažnih mikroskopa znanstvenici otkrivaju riznice znanja i neočekivane tajne čijom bi prilagodbom i primjenom naš svijet mogao postati bolji i održiviji. Ovaj je film putovanje u leptirske nanodimenzije, od visokotehnoloških laboratorija diljem svijeta do najdubljih šumskih predjela i polja lavande, ususret nevjerojatnim vrstama leptira.",
         Category: ["Dokumentarac"],
         Rerun: false,
+        rating: 1,
         channelTitle: "HRT2",
       },
       {
@@ -193,6 +211,7 @@ export const channels = [
           "(Francuska, 2021) Dnevni i noćni leptiri. Ljupki i lijepi, lepršaju i lebde proljetnim i ljetnim nebom. Njihove nježne koreografije u blistavim bojama među najčudesnijima su u životinjskom carstvu. Ali ljepota nije sve što posjeduju. Lećama snažnih mikroskopa znanstvenici otkrivaju riznice znanja i neočekivane tajne čijom bi prilagodbom i primjenom naš svijet mogao postati bolji i održiviji. Ovaj je film putovanje u leptirske nanodimenzije, od visokotehnoloških laboratorija diljem svijeta do najdubljih šumskih predjela i polja lavande, ususret nevjerojatnim vrstama leptira.",
         Category: ["Dokumentarac"],
         Rerun: false,
+        rating: 1,
         channelTitle: "HRT2",
       },
       {
@@ -203,6 +222,7 @@ export const channels = [
           "(Hrvatska, 2023) U dolini Neretve sijeku se svi važni prometni pravci: cesta, pruga i morska luka. Ovdje nema autoceste i sve do krajnjeg juga magistrala je baš kao u slavno doba, žila kucavica na kojoj je dinamičan lokalni život. ",
         Category: ["Dokumentarac", "Informativno"],
         Rerun: false,
+        rating: 1,
         channelTitle: "HRT2",
       },
     ],
@@ -217,6 +237,7 @@ export const channels = [
         description: "(Hrvatska, 2024)",
         Category: ["Informativno"],
         Rerun: false,
+        rating: 1,
         channelTitle: "RTL",
       },
       {
@@ -226,6 +247,7 @@ export const channels = [
         description: "(Hrvatska)",
         Category: ["Diječiji", "Edukativno"],
         Rerun: false,
+        rating: 1,
         channelTitle: "RTL",
       },
       {
@@ -236,6 +258,7 @@ export const channels = [
           "(Japan) Yu-Gi-Oh! Zexal japanska je animirana serija s popularnim japanskim mangama čija se radnja odvija u bliskoj budućnosti u Heartland Cityu. Yuma Tsukumo glavni je lik, mladi duelist koji želi sudjelovati u dvoboju čudovišta te postati prvak igre. Yuma sudjeluje u mnogim dvobojima te se svim snagama trudi postati što bolji igrač kako bi ostvario svoj san o uspjehu.",
         Category: ["Diječiji", "Animirani"],
         Rerun: true,
+        rating: 1,
         channelTitle: "RTL",
       },
       {
@@ -245,6 +268,7 @@ export const channels = [
         description: "(Njemačka, Belgija, Francuska, SAD, 2021) ",
         Category: ["Diječiji", "Animirani"],
         Rerun: false,
+        rating: 1,
         channelTitle: "RTL",
       },
       {
@@ -254,6 +278,7 @@ export const channels = [
         description: "(Hrvatska) ",
         Category: ["Zabava", "Kviz"],
         Rerun: true,
+        rating: 1,
         channelTitle: "RTL",
       },
       {
@@ -264,6 +289,7 @@ export const channels = [
           "(Australija) Spin-off britanske inačice, Grand Designs: Australia pokazuje gledateljima napore graditelja koji pokušavaju podići posebne kuće i domove, specijalno osmišljene prema idejama i nacrtima vlasnika te razne izazove i prepreke kojima se suočavaju graditelji. Kroz show nas vodi arhitekt Peter Maddison, koji se sastaje s vlasnicima koji pokazuju svoje originalne zamisli, nakon čega slijedi CAD (Computer Aided Design) s izgledom konstrukcije projekta.",
         Category: ["Zabava"],
         Rerun: true,
+        rating: 1,
         channelTitle: "RTL",
       },
       {
@@ -274,6 +300,7 @@ export const channels = [
           "(SAD, 2015) Snajperist nastavlja terorizirati San Antonio, a Jordan pada u komu nakon moždanog udara. TC i Topher vraćaju se doma, a s njima stiže i Ali, kojemu se stanje pogoršava. Paul razmišlja o riskantnoj operaciji kako bi spasio ruku. U međuvremenu, Ragosa i Paul liječe beskućnicu sa slomljenim zglobom, a Scott otkriva TC-ju da su Jordanine komplikacije u trudnoći uzrokovale moždani udar.",
         Category: ["Drama", "Kviz"],
         Rerun: true,
+        rating: 1,
         channelTitle: "RTL",
       },
       {
@@ -284,6 +311,7 @@ export const channels = [
           "(Hrvatska) Uštedite svoje vrijeme tako što ćete se odlučiti za kupovinu bez stresa, gužve i žurbe, za kupovinu iz fotelje!",
         Category: ["Prodaja"],
         Rerun: false,
+        rating: 1,
         channelTitle: "RTL",
       },
       {
@@ -294,6 +322,7 @@ export const channels = [
           "(Novi poznanici, SAD, 2017) Severide dozna da pacijent koji mora na transplantaciju koštane srži ispunjava uvjete, ali dolazi do komplikacija kada se Severide ozlijedi i ne može primiti anesteziju. Ipak, Severide ne odustaje. U međuvremenu, Louiejev otac bori se za puno skrbništvo, ali Dawson i Casey ne odustaju.",
         Category: ["Prodaja"],
         Rerun: false,
+        rating: 1,
         channelTitle: "RTL",
       },
     ],
@@ -309,16 +338,18 @@ export const channels = [
           "(Tik Tak i Limenko / Da sam barem mišićav, SAD, 2016) Tik Tak i Limenko: Dorothy i družina nađu Tik Taka, još jedno metalno biće koje se pokvarilo pokraj ceste i sad ga treba popraviti! Tik Tak odmah se zaljubi u Limenka kojega smatra velikim junakom. Želi biti baš poput njega, ali to nije u stanju reći jer mu ne funkcionira glasovni mehanizam. On nastoji zadiviti Limenka kojega pak muči hoće li ga Tik Tak zamijeniti. Limenkova je ljubomora na vrhuncu kada Tik Tak spasi ekipu od podivljalih izvora s planine Springfield. ",
         Category: ["Diječije", "Zabava"],
         Rerun: false,
+        rating: 1,
         channelTitle: "NovaTV",
       },
       {
-        title: "Supžva Bob Skockani",
+        title: "Spužva Bob Skockani",
         startTime: "06:55",
         endTime: "07:20",
         description:
           "(Tim morskih superzlikovaca / Paprikaš od burgera, SAD, 2012) Plankton i Čovjek Raža se udružuju i imaju zajednički cilj: prvo osvojiti Rakovu poslasticu, a nakon toga - svijet! Kalamarko daje otkaz u Rakovoj poslastici kako bi radio kod Planktona u Prijateljskoj kantici. Preko noći ju je pretvorio u popularno okupljalište!",
         Category: ["Diječije", "Zabava"],
         Rerun: false,
+        rating: 1,
         channelTitle: "NovaTV",
       },
       {
@@ -329,6 +360,7 @@ export const channels = [
           "(Kućna dostava, Bosna i Hercegovina, 2008) San Remo traži zaposlenika za kućnu dostavu, a Izet (Mustafa Nadarević) je u potrazi za poslom. Kocke su se sklopile i Izet se zapošljava kao dostavljač pizza. Ivana posjećuje oftalmologa i dobije leće s dioptrijom, ali nije vješta u njihovom stavljanju. Dok joj Faruk (Senad Bašić) pokušava pomoći u tome, leća završavaju u čaši s vodom, a potom i u Izetovom trbuhu.",
         Category: ["Zabava"],
         Rerun: false,
+        rating: 1,
         channelTitle: "NovaTV",
       },
       {
@@ -338,6 +370,7 @@ export const channels = [
         description: "",
         Category: ["Zabava"],
         Rerun: true,
+        rating: 1,
         channelTitle: "NovaTV",
       },
       {
@@ -348,6 +381,7 @@ export const channels = [
           "(Hrvatska, 2015) Davida zabavlja Ivina novootkrivena zainteresiranost za astrologiju. Naime, Iva sve odgovore o njihovoj vezi pronalazi kod gospođe Esmeralde. S druge strane, Ivana nimalo ne zabavlja Dubravkino ispitivanje o tome kakav pogreb bi Ivan htio i želi li donirati organe u slučaju smrti. Marija odluči prodati stare stvari iz podruma i Stjepan se složi s njom, no ubrzo će promijeniti mišljenje.",
         Category: ["Zabava"],
         Rerun: false,
+        rating: 1,
         channelTitle: "NovaTV",
       },
       {
@@ -358,6 +392,7 @@ export const channels = [
           "(Hrvatska) Ponude TV izloga potražite na teletextu NovaTV-a na stranici 230!",
         Category: ["Prodaja"],
         Rerun: true,
+        rating: 1,
         channelTitle: "NovaTV",
       },
       {
@@ -368,6 +403,7 @@ export const channels = [
           "(Hrvatska, 2024) Među brojnim zabavnim formatima Nove TV jedan od dugovječnijih je upravo popularan IN magazin kojeg pratimo od početka 2009., a nastaje pod uredničkom palicom glavne urednice Ivane Mandić.",
         Category: ["Zabava"],
         Rerun: false,
+        rating: 1,
         channelTitle: "NovaTV",
       },
       {
@@ -378,6 +414,7 @@ export const channels = [
           "(Hrvatska) Ponude TV izloga potražite na teletextu NovaTV-a na stranici 230!",
         Category: ["Prodaja"],
         Rerun: true,
+        rating: 1,
         channelTitle: "NovaTV",
       },
       {
@@ -388,6 +425,7 @@ export const channels = [
           "(Turska, 2022) Zejnep, koja slučajno sazna da se Halil vratio kući, osjeća se prevarenom. Svi osim nje svjesni su njegova povratka. Drugo razočarenje doživljava kada sazna da je Ali Fuat povukao slučaj. Iako Zeynep kaže da su se pomirili, iznenada dolazi u Alijev Fuatov dom kako bi otkrila pravi razlog ove iznenadne odluke.",
         Category: ["Drama", "Turska"],
         Rerun: true,
+        rating: 1,
         channelTitle: "NovaTV",
       },
       {
@@ -398,6 +436,7 @@ export const channels = [
           "(Turska) Asli i Mehmet rade zajedno kako bi spasili Minu, dok Hatidže smišlja plan za zaštitu svoje obitelji. Nevzat se osjeća sve sigurnijim u svojim mračnim namjerama, dok se Selčuk pita gdje mu je mjesto u cijeloj priči. Dok se situacija u kući Karli sve više zakomplicira, istina se polako razotkriva, a posljedice će biti duboke i dalekosežne.",
         Category: ["Drama", "Turska"],
         Rerun: true,
+        rating: 1,
         channelTitle: "NovaTV",
       },
     ],
